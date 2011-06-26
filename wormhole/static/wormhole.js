44298a1df7d17bf7306ce9d29a83ca05428fa6dc
Ignacio Thayer <ignacio@cookie.(none)>
2011-06-26 13:45:56 -0700

    Add resolve function

--- wormhole/static/wormhole.js
+++ wormhole/static/wormhole.js
@@ -2,21 +2,37 @@
       function Wormhole() {
 	  var _this = this;
 	  
-	  function handleSuccess(userCallback, result) { 
+	  // These functions handle transport success/error. A
+	  // transport error occurs when the server cannot be reached,
+	  // or returns an error code. Transport success may still
+	  // carry an application error.
+	  function handleTransportSuccess(userCallback, result) { 
 
 	  };
 
-	  function handleError(userCallback, result) { 
+	  function handleTransportError(userCallback, result) { 
 	      
 	  };
 
+	  // Execute a function, and call the callback with the result.
 	  this.call = function(name, args, cb) { 
 	      $.ajax({ type    : "POST",
 		       url     : "/wormhole/call",
-		       success : function(r) { return handleSuccess(cb, r); },
-		       error   : function(r) { return handleError(cb, r); },
+		       success : function(r) { return handleTransportSuccess(cb, r); },
+		       error   : function(r) { return handleTransportError(cb, r); },
 		       dataType: "json" });
 	  };
+	  
+	  // Resolve urls according to django scheme.  'cb' will
+	  // either be called with a WormholeResult object, whose
+	  // content will contain the url name, or error flag will be set if not found.
+	  this.resolve = function(name, args, cb) { 
+	      $.ajax({ type    : "POST",
+		       url     : "/wormhole/resolve",
+		       success : function(r) { return handleTransportSuccess(cb, r); },
+		       error   : function(r) { return handleTransportError(cb, r); },
+		       dataType: "json" });
+	  }
       };
       $.wormhole = new Wormhole();
   })(jQuery);
