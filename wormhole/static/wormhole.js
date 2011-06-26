61f12e1bd228af0f56137d4068b069ff86c1b4cf
Ignacio Thayer <ignacio@cookie.(none)>
2011-06-26 13:26:51 -0700

    Add handleSuccess/handleError functions

--- wormhole/static/wormhole.js
+++ wormhole/static/wormhole.js
@@ -2,10 +2,21 @@
       function Wormhole() {
 	  var _this = this;
 	  
-	  this.call = function(name, args, cb) { 
+	  function handleSuccess(userCallback, result) { 
 
+	  };
+
+	  function handleError(userCallback, result) { 
+	      
+	  };
+
+	  this.call = function(name, args, cb) { 
+	      $.ajax({ type    : "POST",
+		       url     : "/wormhole/call",
+		       success : function(r) { return handleSuccess(cb, r); },
+		       error   : function(r) { return handleError(cb, r); },
+		       dataType: "json" });
 	  };
       };
       $.wormhole = new Wormhole();
-      
   })(jQuery);
