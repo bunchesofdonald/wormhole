4ec2e0fe71cd558f997d523d78a464cae3525658
Chris Pickett <cpickett@zodas.com>
2011-06-26 19:28:57 -0400

    Cleaned up WormholeCall.py, added documentation.

--- wormhole/WormholeCall.py
+++ wormhole/WormholeCall.py
@@ -10,18 +10,57 @@ class WormholeCall(object):
     def __init__(self):
         '''
         init a WormholeCall object
+
+        methods:
+            register - A decorator. Adds the function to Wormholes callbacks.
+            call - Gets a request, looks up the function to call in the
+                callbacks. Returns an HttpResponse containing the result of 
+                the function
+            
+            resolve - A proxy to django.reverse. 
         '''
         self.callbacks = {}
 
     def register(self, func):
+        '''
+        Registers the given function with Wormhole callbacks.
+
+        Used as a decorator:
+            @wormhole.register
+            def sleep(seconds):
+                time.sleep(seconds)
+                return True
+        '''
+        #TODO - Make this more than just a decorator, so that
+        #       wormhole.register(some_function) is supported.
+
         self.callbacks[func.__name__] = func
 
     def call(self, request):
-        wormhole_json = {}
+        '''
+        Calls the given function and returns an HttpResponse with a json 
+        object containing two attrubutes:
+            result : The return result of the function
+            status : 'ok' or 'error'
+            errors : Error detail
+            
+            {"status": "ok", "result": "true", "errors": [] }
+            {"status": "error", "result": "true", "errors": [... list of errors... ]  }
 
-        #if "name" not in request.POST:
-         #   wormhole_json['status'] = errors.WORMHOLE_ERROR
-          #  wormhole_json['error'] = errors.WORMHOLE_FUNCTION_NAME_NOT_FOUND
+        Expects two parameters via POST:
+            name : The name of the function to call
+            args : A Json object of the kwargs to relay to the function
+        '''
+
+        wormhole_json = {
+            'status' : 'ok',
+            'errors' : [],
+            'result' : None
+        }
+
+        if "name" not in request.POST:
+            wormhole_json['status'] = errors.WORMHOLE_ERROR
+            wormhole_json['error'].append(errors.WORMHOLE_FUNCTION_NAME_NOT_FOUND)
         
         function_name = request.POST.get('name')
         json_args = simplejson.loads(request.POST.get('args'))
@@ -29,17 +68,33 @@ class WormholeCall(object):
         function_kwargs = {}
         for arg in json_args:
             # TODO: try/except on the translation to string.
-            function_kwargs[str(arg)] = json_args[arg]
+            try:
+                function_kwargs[str(arg)] = json_args[arg]
+            except:
+                wormhole_json['status'] = errors.WORMHOLE_ERROR
+                wormhole_json['error'].append(errors.WORMHOLE_FUNCTION_NAME_NOT_VALID)
 
         result = self.callbacks[function_name](request, **function_kwargs)
 
         if result:
-            wormhole_json['status'] = 'ok'
             wormhole_json['result'] = result
+        else:
+            wormhole_json['status'] = errors.WORMHOLE_ERROR
+            wormhole_json['error'].append(errors.WORMHOLE_NO_RESULT)
         
         return HttpResponse(simplejson.dumps(wormhole_json), mimetype="application/json") 
 
     def resolve(self, request):
+        '''
+        A proxy to django.reverse
+
+        POST Params:
+            name : Function name or URL Pattern name of view
+            [args] : Optional kwargs to pass to reverse
+
+        Returns an HttpResponse with a json object:
+            {"status": "ok", "result": "/index/" }
+        '''
         view_name = request.POST.get('name')
         try:
             json_args = simplejson.loads(request.POST.get('args', ""))
@@ -49,4 +104,3 @@ class WormholeCall(object):
         view_url = reverse(view_name, kwargs=json_args)
         
         return HttpResponse(simplejson.dumps({'status': 'ok', 'result': view_url}))
-
--- wormhole/errors.py
+++ wormhole/errors.py
@@ -1,2 +1,4 @@
 WORMHOLE_ERROR = 'error'
-WORMHOLE_FUNCTION_NAME_NOT_FOUND = 'A function name must be specified.'
+WORMHOLE_FUNCTION_NAME_NOT_FOUND = ('NAME_NOT_FOUND', 'A function name must be specified.')
+WORMHOLE_FUNCTION_NAME_NOT_VALID = ('NAME_NOT_VALID', 'The given function name is not valid.')
+WORMHOLE_NO_RESULT = ('NO_RESULT', 'The function did not return a result.')
