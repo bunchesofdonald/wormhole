1cd999bd55a6cab1342590cb6a566d1785e3689e
Chris Pickett <cpickett@zodas.com>
2011-06-26 17:44:31 -0400

    included status in return

--- wormhole/WormholeCall.py
+++ wormhole/WormholeCall.py
@@ -1,5 +1,8 @@
+from wormhole import errors
+
 from django.utils import simplejson
 from django.http import HttpResponse
+from django.core.urlresolvers import reverse
 
 class WormholeCall(object):
     def __init__(self):
@@ -12,13 +15,30 @@ class WormholeCall(object):
         self.callbacks[func.__name__] = func
 
     def call(self, request):
+        wormhole_json = {}
+
+        if name not in request.POST:
+            wormhole_json['status'] = errors.WORMHOLE_ERROR
+            wormhole_json['error'] = errors.WORMHOLE_FUNCTION_NAME_NOT_FOUND
+
         function_name = request.POST.get('name')
         json_args = simplejson.loads(request.POST.get('kwargs'))['args']
 
         function_kwargs = {}
         for arg in json_args:
+            # TODO: try/except on the translation to string.
             function_kwargs[str(arg)] = json_args[arg]
 
         result = self.callbacks[function_name](request, **function_kwargs)
+
+        if result:
+            wormhole_json['status'] = 'ok'
+            wormhole_json['result'] = result
         
-        return HttpResponse(simplejson.dumps(result), mimetype="application/json") 
+        return HttpResponse(simplejson.dumps(wormhole_json), mimetype="application/json") 
+
+    def resolve(request):
+        # reverse(viewname[, urlconf=None, args=None, kwargs=None, current_app=None])
+
+
+        pass
---
+++ wormhole/errors.py
@@ -0,0 +1,2 @@
+WORMHOLE_ERROR = 'error'
+WORMHOLE_FUNCTION_NAME_NOT_FOUND = 'A function name must be specified.'
